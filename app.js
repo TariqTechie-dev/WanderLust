92b0c686ea2b5fa0fdeab76b9b2ea77d37dc096d
TariqTechie-dev <tariqtech843@gmail.com>
2026-06-27 20:59:00 +0500

    port change

--- app.js
+++ app.js
@@ -98,6 +98,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error.ejs", { message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
